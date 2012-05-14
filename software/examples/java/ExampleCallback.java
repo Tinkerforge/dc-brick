ac833fce4db99f0f1f365f87b3675a8ed06385ba
Matthias Bolte <matthias@tinkerforge.com>
2012-05-14 18:41:15 +0200

    java: Remove unnecessary new String(...) calls

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -2,15 +2,15 @@ import com.tinkerforge.BrickDC;
 import com.tinkerforge.IPConnection;
 
 public class ExampleCallback {
-	private static final String host = new String("localhost");
+	private static final String host = "localhost";
 	private static final int port = 4223;
-	private static final String UID = new String("aetiNB3mX2u"); // Change to your UID
+	private static final String UID = "aetiNB3mX2u"; // Change to your UID
 
 	// Declare dc static, so the listener can use it. In a real program you probably
 	// want to make a real listener class (not the anonym inner class) and pass the dc
 	// reference to it. But we want to keep the examples as short es possible.
 	static BrickDC dc;
-	
+
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the commnents below
 	public static void main(String args[]) throws Exception {
