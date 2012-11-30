74a6d3be1135178b20a648e5076eb43916c70364
Matthias Bolte <matthias@tinkerforge.com>
2012-11-30 16:18:21 +0100

    delphi: Remove unnecessary parentheses and call destructor again

--- software/examples/delphi/ExampleConfiguration.pas
+++ software/examples/delphi/ExampleConfiguration.pas
@@ -26,7 +26,7 @@ var
 procedure TExample.Execute;
 begin
   { Create IP connection }
-  ipcon := TIPConnection.Create();
+  ipcon := TIPConnection.Create;
 
   { Create device object }
   dc := TBrickDC.Create(UID, ipcon);
@@ -44,6 +44,7 @@ begin
 
   WriteLn('Press key to exit');
   ReadLn;
+  ipcon.Destroy;
 end;
 
 begin
