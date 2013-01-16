3eb8f063805eb471e6432a1ee07b404f6be2ff89
Matthias Bolte <matthias@tinkerforge.com>
2013-01-16 20:18:21 +0100

    Update Java example and use a final local instead of a global static variable

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -1,21 +1,19 @@
 import com.tinkerforge.BrickDC;
 import com.tinkerforge.IPConnection;
+import com.tinkerforge.TimeoutException;
+import com.tinkerforge.NotConnectedException;
 
 public class ExampleCallback {
 	private static final String host = "localhost";
 	private static final int port = 4223;
 	private static final String UID = "aetiNB3mX2u"; // Change to your UID
 
-	// Declare dc static, so the listener can use it. In a real program you probably
-	// want to make a real listener class (not the anonym inner class) and pass the dc
-	// reference to it. But we want to keep the examples as short es possible.
-	static BrickDC dc;
-
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
-		dc = new BrickDC(UID, ipcon); // Create device object
+		// Note: Declare servo final, so the listener can access it
+		final BrickDC dc = new BrickDC(UID, ipcon); // Create device object
 
 		ipcon.connect(host, port); // Connect to brickd
 		// Don't use device before ipcon is connected
@@ -27,16 +25,16 @@ public class ExampleCallback {
 				if(velocity == 32767) {
 					System.out.println("Velocity: Full Speed forward, turning backward");
 					try {
-						ExampleCallback.dc.setVelocity((short)-32767);
-					} catch(IPConnection.TimeoutException e) {
-					} catch(IPConnection.NotConnectedException e) {
+						dc.setVelocity((short)-32767);
+					} catch(TimeoutException e) {
+					} catch(NotConnectedException e) {
 					}
 				} else if(velocity == -32767) {
 					System.out.println("Velocity: Full Speed backward, turning forward");
 					try {
-						ExampleCallback.dc.setVelocity((short)32767);
-					} catch(IPConnection.TimeoutException e) {
-					} catch(IPConnection.NotConnectedException e) {
+						dc.setVelocity((short)32767);
+					} catch(TimeoutException e) {
+					} catch(NotConnectedException e) {
 					}
 				} else {
 					System.out.println("Error"); // Can only happen if another program sets velocity
