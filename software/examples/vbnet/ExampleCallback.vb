1424cdd36444da18987380488a40865c0da09e29
Matthias Bolte <matthias@tinkerforge.com>
2015-08-18 15:07:27 +0200

    Cleanup VB.NET examples

--- software/examples/vbnet/ExampleCallback.vb
+++ software/examples/vbnet/ExampleCallback.vb
@@ -3,7 +3,7 @@ Imports Tinkerforge
 Module ExampleCallback
     Const HOST As String = "localhost"
     Const PORT As Integer = 4223
-    Const UID As String = "6eS9CJ" ' Change to your UID
+    Const UID As String = "XYZ" ' Change to your UID
 
     ' Use velocity reached callback to swing back and forth between
     ' full speed forward and full speed backward
--- software/examples/vbnet/ExampleConfiguration.vb
+++ software/examples/vbnet/ExampleConfiguration.vb
@@ -3,7 +3,7 @@ Imports Tinkerforge
 Module ExampleConfiguration
     Const HOST As String = "localhost"
     Const PORT As Integer = 4223
-    Const UID As String = "6eS9CJ" ' Change to your UID
+    Const UID As String = "XYZ" ' Change to your UID
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
