1d17548c79f3c0a69a2b7e9dee33209c40e2045c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:19:56 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{dc_brick::*, ipconnection::IpConnection};
+use tinkerforge::{dc_brick::*, ip_connection::IpConnection};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -7,30 +7,30 @@ const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your DC Brick
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let dc_brick = DCBrick::new(UID, &ipcon); // Create device object
+    let dc = DcBrick::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
     // The acceleration has to be smaller or equal to the maximum
     // acceleration of the DC motor, otherwise the velocity reached
     // callback will be called too early
-    dc_brick.set_acceleration(5000); // Slow acceleration
-    dc_brick.set_velocity(32767); // Full speed forward
+    dc.set_acceleration(5000); // Slow acceleration
+    dc.set_velocity(32767); // Full speed forward
 
-    //Create listener for velocity reached events.
-    let velocity_reached_listener = dc_brick.get_velocity_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the dc_brick
+    //Create receiver for velocity reached events.
+    let velocity_reached_receiver = dc.get_velocity_reached_receiver();
+    // Spawn thread to handle received events. This thread ends when the dc
     // is dropped, so there is no need for manual cleanup.
-    let dc_brick_copy = dc_brick.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
+    let dc_copy = dc.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
-        for event in velocity_reached_listener {
-            if event == 32767 {
+        for velocity_reached in velocity_reached_receiver {
+            if velocity_reached == 32767 {
                 println!("Velocity: Full speed forward, now turning backward");
-                dc_brick_copy.set_velocity(-32767);
-            } else if event == -32767 {
+                dc_copy.set_velocity(-32767);
+            } else if velocity_reached == -32767 {
                 println!("Velocity: Full speed backward, now turning forward");
-                dc_brick_copy.set_velocity(32767);
+                dc_copy.set_velocity(32767);
             } else {
                 //can only happen if another program sets velocity
                 panic!("Error");
@@ -39,12 +39,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Enable motor power
-    dc_brick.enable();
+    dc.enable();
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    dc_brick.disable(); // Disable motor power
+    dc.disable(); // Disable motor power
     ipcon.disconnect();
     Ok(())
 }
--- software/examples/rust/example_configuration.rs
+++ software/examples/rust/example_configuration.rs
@@ -1,28 +1,28 @@
 use std::{error::Error, io};
 
-use tinkerforge::{dc_brick::*, ipconnection::IpConnection};
+use tinkerforge::{dc_brick::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your DC Brick
+const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your DC Brick.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let dc_brick = DCBrick::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let dc = DcBrick::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    dc_brick.set_drive_mode(DC_BRICK_DRIVE_MODE_DRIVE_COAST);
-    dc_brick.set_pwm_frequency(10000); // Use PWM frequency of 10kHz
-    dc_brick.set_acceleration(5000); // Slow acceleration
-    dc_brick.set_velocity(32767); // Full speed forward
-    dc_brick.enable(); // Enable motor power
+    dc.set_drive_mode(DC_BRICK_DRIVE_MODE_DRIVE_COAST);
+    dc.set_pwm_frequency(10000); // Use PWM frequency of 10kHz
+    dc.set_acceleration(5000); // Slow acceleration
+    dc.set_velocity(32767); // Full speed forward
+    dc.enable(); // Enable motor power
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    dc_brick.disable(); // Disable motor power
+    dc.disable(); // Disable motor power
     ipcon.disconnect();
     Ok(())
 }
